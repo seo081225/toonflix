8bfdec1dc1ada722d542acc10bc79c4fa00d3e90
seo081225 <121672963+seo081225@users.noreply.github.com>
2023-08-19 00:27:35 +0900

    #6.2 Data Fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home.dart';
+import 'package:toonflix/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
